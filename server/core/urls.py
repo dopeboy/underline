386ecad734b98a92e990430cea84577ca2c12ba7
Manish Sinha <arithmetic@gmail.com>
2021-06-08 09:15:48 -0400

    fix error with sync

--- server/core/urls.py
+++ server/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, re_path, include
-from .views import SyncGames, SyncPlayers, SyncLines, RunWholeShebang, Test
+from .views import SyncGames, SyncPlayers, SyncLines, RunWholeShebang
 
 
 urlpatterns = [
@@ -7,5 +7,4 @@ urlpatterns = [
     path("sync_games", SyncGames.as_view(), name="sync_games"),
     path("sync_lines", SyncLines.as_view(), name="sync_lines"),
     path("run_whole_shebang", RunWholeShebang.as_view(), name="run_whole_shebang"),
-    path("test", Test.as_view(), name="test"),
 ]
